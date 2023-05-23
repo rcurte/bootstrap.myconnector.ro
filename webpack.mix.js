7a44b12062dbc3772a792d3110f5fef3061ce50c
Marius Constantin <marius.constantin@universum.ro>
2023-05-23 20:37:55 +0300

    light mode

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,5 +8,7 @@ mix.webpackConfig({
 
 mix.js('src/js/main.js', 'assets/js')
 .sass('src/scss/main.scss', 'assets/css')
+.sass('src/scss/default.scss', 'assets/css')
+.sass('src/scss/light.scss', 'assets/css')
 .copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'assets/webfonts')
 .copy('node_modules/@fortawesome/fontawesome-free/css/all.min.css', 'assets/css');
